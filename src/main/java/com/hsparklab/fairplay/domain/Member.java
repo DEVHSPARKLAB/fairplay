ce35c755ab4a6c81ce62bbb6fdcd7403763ab21c
DEVHSPARKLAB <dev.hsparklab@gmail.com>
2024-10-07 17:44:23 +0900

    2024.10.07

--- src/main/java/com/hsparklab/fairplay/domain/Member.java
+++ src/main/java/com/hsparklab/fairplay/domain/Member.java
@@ -1,15 +1,15 @@
-package com.hsparklab.fairplay.domain;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Table (name = "member")
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
-
-}
+//package com.hsparklab.fairplay.domain;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+//import lombok.AccessLevel;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Entity
+//@Table (name = "member")
+//@Getter
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//public class Member {
+//
+//}
