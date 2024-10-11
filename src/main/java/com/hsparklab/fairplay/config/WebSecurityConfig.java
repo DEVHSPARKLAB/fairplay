454b49147a0241353313508da39114e17d9cd999
DEVHSPARKLAB <dev.hsparklab@gmail.com>
2024-10-11 16:46:42 +0900

    2024.10.11

--- src/main/java/com/hsparklab/fairplay/config/WebSecurityConfig.java
+++ src/main/java/com/hsparklab/fairplay/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.hsparklab.fairplay.config;
 
+import com.hsparklab.fairplay.config.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -30,6 +32,7 @@ public class WebSecurityConfig {
     }
 
     @Bean
+    //BCryptPasswordEncoder
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
@@ -38,24 +41,22 @@ public class WebSecurityConfig {
     //특정 HTTP 요청에 대한 웹 기반 보안
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(AbstractHttpConfigurer::disable) //csrf disalbe
+                .httpBasic(AbstractHttpConfigurer::disable) //httpbasic disable (특정리소스 접근 시 httpbasic 인증 요청창 미사용)
+                .formLogin(AbstractHttpConfigurer::disable) //form login disable
+                .logout(AbstractHttpConfigurer::disable) //logout disable
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 new AntPathRequestMatcher("/login"),
                                 new AntPathRequestMatcher("/signup"),
-                                new AntPathRequestMatcher("/user")
+                                new AntPathRequestMatcher("/")
                         ).permitAll()
                         .anyRequest().authenticated())
-                .formLogin(formLogin -> formLogin
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/")
-                )
-                .logout(logout -> logout
-                        .logoutSuccessUrl("/login")
-                        .invalidateHttpSession(true)
-                )
-                .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
+
+
 
 
 }
